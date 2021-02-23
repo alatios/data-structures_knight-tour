32826221ea2ab3e66295af88877e8956e7fd227e
Alessandro De Gennaro <alessandro.degennaro+github@protonmail.com>
2021-02-23 17:06:06 +0100

    Remove line connecting dots in plots

--- analyze_results.py
+++ analyze_results.py
@@ -6,14 +6,14 @@ inputFile = "time_results.txt"
 data = pd.read_table(inputFile, delim_whitespace=True)
 data.sort_values(by='n', inplace=True, ignore_index=True)
 
-plt.plot(data['n']**2,data['time'], marker="x", color='orange')
+plt.plot(data['n']**2,data['time'], marker="x", color='orange', linewidth=0)
 plt.xlabel("$n^2$")
 plt.ylabel("Time [seconds]")
 plt.grid()
 plt.savefig("time_vs_n2.pdf", bbox_inches='tight')
 plt.close()
 
-plt.plot(data['n'],data['time'], marker="x", color='orange')
+plt.plot(data['n'],data['time'], marker="x", color='orange', linewidth=0)
 plt.xlabel("$n$")
 plt.ylabel("Time [seconds]")
 plt.grid()
