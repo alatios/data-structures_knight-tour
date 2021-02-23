fba5ea14f717ca2bd29b2cc85389792ead825008
Alessandro De Gennaro <alessandro.degennaro+github@protonmail.com>
2021-02-23 16:54:30 +0100

    Add python3 script for time vs. n plots

---
+++ analyze_results.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+inputFile = "time_results.txt"
+data = pd.read_table(inputFile, delim_whitespace=True)
+data.sort_values(by='n', inplace=True, ignore_index=True)
+
+plt.plot(data['n']**2,data['time'], marker="x", color='orange')
+plt.xlabel("$n^2$")
+plt.ylabel("Time [seconds]")
+plt.grid()
+plt.savefig("time_vs_n2.pdf", bbox_inches='tight')
+plt.close()
+
+plt.plot(data['n'],data['time'], marker="x", color='orange')
+plt.xlabel("$n$")
+plt.ylabel("Time [seconds]")
+plt.grid()
+plt.savefig("time_vs_n.pdf", bbox_inches='tight')
+plt.close()
+
+print("Analysis complete!")
+exit(0)
